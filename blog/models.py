6d6b253b85e5b035878c6885f84fd86a186770fb
Richard DiCorcia <34862761+radicor48@users.noreply.github.com>
2020-03-23 10:18:01 -0400

    added home page and more

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
 class Blog(models.Model):
+    title = models.CharField(max_length=50, blank=True)
+    pub_date = models.DateTimeField()
+    body = models.TextField(blank = True)
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,10 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Job
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Jobs index.")
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
